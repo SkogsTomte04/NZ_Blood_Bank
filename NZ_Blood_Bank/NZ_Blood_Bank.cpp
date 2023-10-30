bc22f8a8442eea3b34e38ad044b6d36d66d751ed
ShockCharge <jeznerpogi@gmail.com>
2023-10-30 14:05:24 +1300

    Made it a little fancier i guess? lol

--- NZ_Blood_Bank/NZ_Blood_Bank.cpp
+++ NZ_Blood_Bank/NZ_Blood_Bank.cpp
@@ -6,14 +6,14 @@ using namespace std;
 
 int main()
 {
-    cout << colors::red << "========================================" << colors::reset << endl;
+    cout << colors::red << "==========================================" << colors::reset << endl;
     cout << colors::bright_red << "\tNew Zealand Blood Bank" << colors::reset << endl;
-    cout << colors::bright_grey << "Welcome from the largest blood bank in New Zealand," << colors::reset << endl;
-    cout << colors::bright_grey << "   where blood donors donate to save lives." << colors::reset << endl;
+    cout << colors::bright_grey << "Welcome from the largest blood bank in NZ," << colors::reset << endl;
+    cout << colors::bright_grey << " where blood donors donate to save lives." << colors::reset << endl;
     cout << endl;
     cout << colors::white << "\t      Login" << colors::reset << endl;
     cout << colors::white << "\t     Register" << colors::reset << endl;
-    cout << colors::red << "========================================" << colors::reset << endl;
+    cout << colors::red << "==========================================" << colors::reset << endl;
 
     return 0;
 }
