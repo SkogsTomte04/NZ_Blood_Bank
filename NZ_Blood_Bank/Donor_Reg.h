ce6e08000f11e18304e02e8ab2de318f1a25d952
William Möller <williammoller321@gmail.com>
2023-11-01 01:16:23 +1300

    Now usable
    collects all user info and generates a new user in the "user_data.json" folder

--- NZ_Blood_Bank/Donor_Reg.h
+++ NZ_Blood_Bank/Donor_Reg.h
@@ -7,27 +7,29 @@
 using json = nlohmann::json;
 using namespace std;
 
-string FileArray[13] = { "First_Name", "Last_Name", "dob", "Nationality", "Ethnicity", "Med_Conditions", "Blood_Group", "Contact_no", "Email", "Adress", "Prev_Donation_Date", "Username", "Password" };
-json Doc;
+string FileArray[12] = { "First_Name", "Last_Name", "dob", "Nationality", "Ethnicity", "Med_Conditions", "Blood_Group", "Contact_no", "Email", "Adress", "Prev_Donation_Date", "Password" };
+
 
 void Register() {
-	
+	ifstream in("user_data.json");
+
+	json Doc = json::parse(in);
+
 	string userInput, username;
 	cout << "Enter a Username: ";
-	cin >> username;
-	getline(cin, userInput);
+	getline(cin, username);
 
-	Doc["users"][username];
+	Doc[username];
+	Doc[username]["Username"] = username;
 	
 	for (int i = 0; i < size(FileArray); i++) {
 		cout << FileArray[i] << ": ";
 		getline(cin, userInput);
 
-		Doc["users"][username][FileArray[i]] = userInput;
+		Doc[username][FileArray[i]] = userInput;
 	}
-	
-	std::fstream File;
-	File.open("user_data.json", std::ios::out);
-
-	File << std::setw(4) << Doc;
+	fstream file;
+	file.open("user_data.json", std::ios::out);
+	file << std::setw(4) << Doc;
+	file.close();
 }
