fd0c98a96864733dc7a93280b34680c22c3914a0
William Möller <williammoller321@gmail.com>
2023-10-31 16:25:56 +1300

    User Registration now works
    *partially

--- NZ_Blood_Bank/Donor_Reg.h
+++ NZ_Blood_Bank/Donor_Reg.h
@@ -1,27 +1,33 @@
 #pragma once
 #include <iostream>
 #include <fstream>
+#include <vector>
 #include "json.hpp"
 
 using json = nlohmann::json;
-
 using namespace std;
 
-
 string FileArray[13] = { "First_Name", "Last_Name", "dob", "Nationality", "Ethnicity", "Med_Conditions", "Blood_Group", "Contact_no", "Email", "Adress", "Prev_Donation_Date", "Username", "Password" };
+json Doc;
 
 void Register() {
 	
-	string userInput;
-	json j;
+	string userInput, username;
+	cout << "Enter a Username: ";
+	cin >> username;
+	getline(cin, userInput);
 
-
+	Doc["users"][username];
+	
 	for (int i = 0; i < size(FileArray); i++) {
 		cout << FileArray[i] << ": ";
 		getline(cin, userInput);
-		j[FileArray[i]] = userInput;
+
+		Doc["users"][username][FileArray[i]] = userInput;
 	}
-	ofstream jsonOut("./user_data.json");
-	jsonOut << std::setw(4) << j << std::endl;
 	
+	std::fstream File;
+	File.open("user_data.json", std::ios::out);
+
+	File << std::setw(4) << Doc;
 }
