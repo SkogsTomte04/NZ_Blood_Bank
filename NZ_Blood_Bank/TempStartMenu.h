5b85a2666638d93d79706c76573d3f47c2512788
William Möller <williammoller321@gmail.com>
2023-11-02 02:21:06 +1300

    rewrote lost code
    prep for "Appointment function" creation

--- NZ_Blood_Bank/TempStartMenu.h
+++ NZ_Blood_Bank/TempStartMenu.h
@@ -6,20 +6,35 @@
 using namespace std;
 
 void start(bool& loggedIn, string& user) {
-    int userinput;
+    
+    while (!loggedIn) {
+        int userinput;
 
-    cout << "1. Log In\n2. Sign up" << endl;
-    cin >> userinput;
+        cout << "1. Log In\n2. Sign up" << endl;
+        cin >> userinput;
 
-    if (userinput == 1) {
-        string status = LogIn();
-        if (status != "exe.fail") {
-            user = status;
-            loggedIn = true;
+        if (userinput == 1) {
+            string status = LogIn();
+            if (status != "exe.fail") {
+                user = status;
+                loggedIn = true;
+            }
+
         }
-
+        if (userinput == 2) {
+            Register();
+        }
     }
-    if (userinput == 2) {
-        Register();
+    
+    while (loggedIn) {
+        int userinput;
+        cout << "1. Book appointment\n2. Log out" << endl;
+        cin >> userinput;
+        if (userinput == 1) {
+            cout << "404 nothing here" << endl;
+        }
+        if (userinput == 2) {
+            loggedIn = false;
+        }
     }
 }
