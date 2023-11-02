d00100dc6534c8e7619e0230740c683e1d32eaba
William Möller <williammoller321@gmail.com>
2023-11-02 14:28:36 +1300

    refactored booking.h

--- NZ_Blood_Bank/Booking.h
+++ NZ_Blood_Bank/Booking.h
@@ -1,43 +1,14 @@
 #pragma once
-#pragma once
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <array>
 #include "json.hpp"
-using json = nlohmann::json;
-using namespace std;
+//using json = nlohmann::json;
+//using namespace std;
 
-string weekdays[5] = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" };
+int getHour();
 
-int getHour() {
-	return 0;
-}
+std::string getDay();
 
-string getDay() {
-	int userinput;
-
-	for (int i = 1; i <= size(weekdays); i++) {
-		cout << i << ". " << weekdays[i - 1] << endl;
-	}
-	cout << "Pick a day to Donate" << endl;
-	cin >> userinput;
-
-	return weekdays[userinput - 1];
-}
-
-void getAvailable() {
-
-}
-
-void BookAppointment(string user) {
-	ifstream in("user_data.json");
-	json Doc = json::parse(in);
-	string day = getDay();
-
-
-
-	fstream file;
-	file.open("user_data.json", std::ios::out);
-	file << std::setw(4) << Doc;
-	file.close();
-}
+void BookAppointment(std::string user);
