7f23558688c84900c965d1dfc9089f9d91234485
William Möller <williammoller321@gmail.com>
2023-11-05 02:48:16 +1300

    Removed unnecessary code

--- NZ_Blood_Bank/NZ_Blood_Bank.cpp
+++ NZ_Blood_Bank/NZ_Blood_Bank.cpp
@@ -1,15 +1,47 @@
 #include <iostream>
 #include "Donor_Reg.h"
 #include "LogIn.h"
-#include "TempStartMenu.h"
+#include "Booking.h"
+#include "intro_screen.h"
 
 
 int main()
 {
-    bool isLoggedin = false;
+    bool loggedIn = false;
     string activeUser;
 
-    while (!isLoggedin) {
-        start(isLoggedin, activeUser);
+    while (!loggedIn) { // does this if user is not logged in
+        system("cls");
+
+        draw_intro();
+
+        int userinput;
+        cin >> userinput;
+
+        if (userinput == 1) {
+            string loginStatus = LogIn();
+            if (loginStatus != "exe.fail") {
+                activeUser = loginStatus;
+                loggedIn = true;
+            }
+
+        }
+        if (userinput == 2) {
+            Register();
+        }
+
+    }
+
+    while (loggedIn) { // does this if user is logged in
+        system("cls");
+        int userinput;
+        cout << "1. Book appointment\n2. Log out" << endl;
+        cin >> userinput;
+        if (userinput == 1) {
+            BookAppointment(activeUser);
+        }
+        if (userinput == 2) {
+            loggedIn = false;
+        }
     }
 }
