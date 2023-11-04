ea9e9b3cc35c3bd35cf10522e6d30701b0076f4b
William Möller <williammoller321@gmail.com>
2023-11-05 02:51:02 +1300

    Update NZ_Blood_Bank.cpp

--- NZ_Blood_Bank/NZ_Blood_Bank.cpp
+++ NZ_Blood_Bank/NZ_Blood_Bank.cpp
@@ -7,41 +7,43 @@
 
 int main()
 {
-    bool loggedIn = false;
+    bool isRunning = true, loggedIn = false;
     string activeUser;
 
-    while (!loggedIn) { // does this if user is not logged in
-        system("cls");
+    while (isRunning) {
+        while (!loggedIn) { // does this if user is not logged in
+            system("cls");
 
-        draw_intro();
+            draw_intro();
 
-        int userinput;
-        cin >> userinput;
+            int userinput;
+            cin >> userinput;
 
-        if (userinput == 1) {
-            string loginStatus = LogIn();
-            if (loginStatus != "exe.fail") {
-                activeUser = loginStatus;
-                loggedIn = true;
+            if (userinput == 1) {
+                string loginStatus = LogIn();
+                if (loginStatus != "exe.fail") {
+                    activeUser = loginStatus;
+                    loggedIn = true;
+                }
+
+            }
+            if (userinput == 2) {
+                Register();
             }
 
         }
-        if (userinput == 2) {
-            Register();
-        }
 
-    }
-
-    while (loggedIn) { // does this if user is logged in
-        system("cls");
-        int userinput;
-        cout << "1. Book appointment\n2. Log out" << endl;
-        cin >> userinput;
-        if (userinput == 1) {
-            BookAppointment(activeUser);
-        }
-        if (userinput == 2) {
-            loggedIn = false;
+        while (loggedIn) { // does this if user is logged in
+            system("cls");
+            int userinput;
+            cout << "1. Book appointment\n2. Log out" << endl;
+            cin >> userinput;
+            if (userinput == 1) {
+                BookAppointment(activeUser);
+            }
+            if (userinput == 2) {
+                loggedIn = false;
+            }
         }
     }
 }
