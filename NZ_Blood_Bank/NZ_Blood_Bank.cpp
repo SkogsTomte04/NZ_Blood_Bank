9eec9f189fed4fe5c75a2f11a0b3192ea30434ff
ShockCharge <jeznerpogi@gmail.com>
2023-11-10 14:39:05 +1300

    Finished making a dummy Admin Screen

--- NZ_Blood_Bank/NZ_Blood_Bank.cpp
+++ NZ_Blood_Bank/NZ_Blood_Bank.cpp
@@ -2,12 +2,13 @@
 #include <fstream>
 #include "intro_screen.h"
 #include "recipient_screen.h"
+#include "admin_screen.h"
 
 using namespace std;
 
 int main()
 {
-    intro_screen();
+    admin_screen();
 
     return 0;
 }
